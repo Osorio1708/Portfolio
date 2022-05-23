78ab62c173c429d307e7f398ceee2267d8b4c6ae
Santiago Osorio Quintero <saostero@gmail.com>
2022-05-23 00:43:01 -0500

    some changes

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
+//Libraries
 const express = require('express');
+//Requires
 const routerApi = require('./routes');
 const { logError, errorHandler, wrapError } = require('./middlewares/errorHandlers');
 const notFoundHandler = require('./middlewares/notFoudHandler');
+//Setup
 const app = express();
 const port = 3000;
-
 app.use(express.json());
-
 app.get('/', (req, res) => {
   res.send('Welcome to Porfolio API');
 });
-
 routerApi(app);
-
+//Middlewares
 app.use(notFoundHandler);
 app.use(logError);
 app.use(wrapError);
 app.use(errorHandler);
-
+//Listen port
 app.listen(port, () => {
   console.log('Server is running in port: ' +  port);
   console.log('Go to the page: ' +  `http://localhost:${port}`);
